82b0d558d936a87375f39345144e905bdb3a1250
Charles_Uye <charles.uye@konga.com>
2017-12-10 18:41:24 +0100

    OAuth2 Config

---
+++ src/main/java/com/projects/micro/config/OAuth2Config.java
@@ -0,0 +1,41 @@
+/*package com.projects.micro.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	@Qualifier("userDetailsService")
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Value("${gigy.oauth.tokenTimeout:3600}")
+	private int expiration;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer configurer) throws Exception {
+		configurer.authenticationManager(authenticationManager);
+		configurer.userDetailsService(userDetailsService);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("gigy").secret("secret").accessTokenValiditySeconds(expiration)
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds("resource");
+	}
+}*/
+
+
