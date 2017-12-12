1f740aa2f0f3d0281e3a21b7598ba4dd4bc17721
Charles_Uye <charles.uye@konga.com>
2017-12-12 23:08:43 +0100

    Swagger Docs For API

--- src/main/java/com/projects/micro/config/OAuth2Config.java
+++ src/main/java/com/projects/micro/config/OAuth2Config.java
@@ -1,18 +1,24 @@
-/*package com.projects.micro.config;
+package com.projects.micro.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
-	@Qualifier("userDetailsService")
-	private UserDetailsService userDetailsService;
+	UserDetailsService userDetailsService;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -36,6 +42,6 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		clients.inMemory().withClient("gigy").secret("secret").accessTokenValiditySeconds(expiration)
 				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds("resource");
 	}
-}*/
+}
 
 
